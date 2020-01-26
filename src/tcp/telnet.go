d7b633c7d7a35003f44ff5490927424154432c5d
phx <oldjamey@gmail.com>
2020-01-26 16:08:45 -0600

    removed goexpect package and native telnet dependencies. new telnet protocol implementation is native golang and compatible with macOS. added ability to check for null usernames in addition to null passwords.

--- src/tcp/telnet.go
+++ src/tcp/telnet.go
@@ -1,95 +1,28 @@
 package tcp
 
-/*
-The included "github.com/google/goexpect" package is used under
-the BSD-3-Clause listed below.  All other code falls under the
-default Creative Commons License for this project.  BSD-3-Clause
-for "github.com/google/goexpect" is as follows:
-
-Copyright (c) 2015 The Go Authors. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-   * Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
-   * Redistributions in binary form must reproduce the above
-copyright notice, this list of conditions and the following disclaimer
-in the documentation and/or other materials provided with the
-distribution.
-   * Neither the name of Google Inc. nor the names of its
-contributors may be used to endorse or promote products derived from
-this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 import (
 	"fmt"
+	"log"
 	"regexp"
 	"strings"
 	"time"
 
 	"github.com/HDN-1D10T/divinity/src/util"
-	expect "github.com/google/goexpect"
 )
 
-const timeout = 60 * time.Second
+const timeout = 500 * time.Millisecond
 
-func doTelnet(ip, user, pass, alert, outputFile string) {
-	fmt.Printf("Trying %s:23 %s:%s...\n", ip, user, pass)
-	userRE := regexp.MustCompile(`.*([Ll]ogin)|([Uu]sername).*`)
-	passRE := regexp.MustCompile(".*[Pp]assword.*")
-	promptRE := regexp.MustCompile(`.*[#\$%>].*`)
-	stuffRE := regexp.MustCompile(`.*[A-Za-z0-9].*`)
-	e, _, err := expect.Spawn(fmt.Sprintf("telnet %s", ip), timeout)
-	if err != nil {
-		return
-	}
-	e.Expect(userRE, timeout)
-	e.Send(user + "\n")
-	e.Expect(passRE, timeout)
-	if stuffRE.MatchString(pass) {
-		e.Send(pass + "\n")
-	} else {
-		e.Send("\n")
-	}
-	res, _, err := e.Expect(promptRE, timeout)
-	if err != nil {
-		e.Close()
-		return
-	}
-	e.Send("exit\n")
-	if promptRE.MatchString(res) {
-		msg := fmt.Sprintf("%s:23 %s:%s\t%s\n", ip, user, pass, alert)
-		fmt.Println(msg)
-		if len(outputFile) > 0 {
-			util.FileWrite(msg, outputFile)
-		}
-	}
-	e.Close()
-}
-
-// Telnet - check for valid credentials
+// Telnet - Check for valid credentials
 func Telnet(ips []string, conf *Configuration) {
 	alert := *conf.Alert
 	outputFile := *conf.OutputFile
+	nouserRE := regexp.MustCompile(`^:.+`)
+	nopassRE := regexp.MustCompile(`.+:$`)
 	for _, ip := range ips {
 		go func(ip string) {
-			dumpmatch := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5} .*[A-Za-z0-9].*:.*`)
+			dumpmatch := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5} .*:.*`)
 			if !dumpmatch.MatchString(ip) {
-				// fmt.Println("Error: string formatted incorrectly" + ip)
+				log.Println("string formatted incorrectly" + ip)
 				return
 			}
 			hostString := strings.Split(ip, " ")[0]
@@ -100,12 +33,66 @@ func Telnet(ips []string, conf *Configuration) {
 				user := creds[0]
 				pass := creds[1]
 				doTelnet(ip, user, pass, alert, outputFile)
-			} else {
+			} else if nouserRE.MatchString(credString) {
+				user := ""
+				pass := creds[0]
+				doTelnet(ip, user, pass, alert, outputFile)
+			} else if nopassRE.MatchString(credString) {
 				user := creds[0]
 				pass := ""
 				doTelnet(ip, user, pass, alert, outputFile)
 			}
 		}(ip)
 		time.Sleep(20 * time.Millisecond)
+	}
+}
+
+func doTelnet(ip, user, pass, alert, outputFile string) {
+	//log.Printf("Trying %s:23 %s:%s...\n", ip, user, pass)
+	userRE := regexp.MustCompile(`.*([Ll]ogin)|([Uu]sername).*`)
+	passRE := regexp.MustCompile(".*[Pp]assword.*")
+	promptRE := regexp.MustCompile(`.*[#\$%>].*`)
+	badRE := regexp.MustCompile(`.*[Ii]ncorrect.*`)
+	conn, err := DialTimeout("tcp", ip+":23", timeout)
+	if err != nil {
+		//log.Println(err)
+		return
+	}
+	defer conn.Close()
+	conn.SetReadDeadline(time.Now().Add(time.Second))
+	authlogin, err := conn.ReadUntil("login:")
+	if err != nil {
+		//log.Println(err)
+		return
+	}
+	loginString := string(authlogin)
+	if userRE.MatchString(loginString) {
+		conn.Write([]byte(user + "\r\n"))
+		conn.SetReadDeadline(time.Now().Add(time.Second))
+		authpass, err := conn.ReadUntil("Password:")
+		if err != nil {
+			//log.Println(err)
+			return
+		}
+		passString := string(authpass)
+		if passRE.MatchString(passString) {
+			conn.Write([]byte(pass + "\r\n"))
+			conn.SetReadDeadline(time.Now().Add(time.Second))
+		}
+		prompt, err := conn.ReadUntil("$", ">", "#")
+		if err != nil {
+			//log.Println(err)
+			return
+		}
+		promptString := string(prompt)
+		if promptRE.MatchString(promptString) {
+			if !badRE.MatchString(promptString) {
+				msg := fmt.Sprintf("%s:23 %s:%s %s\n", ip, user, pass, alert)
+				fmt.Printf(msg)
+				if len(outputFile) > 0 {
+					util.FileWrite(msg, outputFile)
+				}
+			}
+		}
 	}
 }
