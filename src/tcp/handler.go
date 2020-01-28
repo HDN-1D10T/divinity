6962c2ecd8b741965a8a1f69f2367a8d17514cb5
phx <oldjamey@gmail.com>
2020-01-28 10:22:55 -0600

    removed 65 lines of code by consolidating -list and -dumplist

--- src/tcp/handler.go
+++ src/tcp/handler.go
@@ -1,6 +1,7 @@
 package tcp
 
 import (
+	"fmt"
 	"log"
 	"regexp"
 	"strings"
@@ -32,67 +33,98 @@ var (
 // Handler for TCP
 // Parses config options and handles as necessary
 func Handler(lines []string) {
-	if len(*Conf.DumpList) > 0 {
-		doDumpList(lines)
-		return
-	}
-	if len(*Conf.List) > 0 {
-		doIPList(lines)
+	if len(*Conf.DumpList) > 0 || len(*Conf.List) > 0 {
+		doList(lines)
 		return
 	}
 }
 
-// Process Dumplist
-func doDumpList(lines []string) {
-	dumplistMatch := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}:[0-9]{1,5} .*:.*`)
+func doList(lines []string) {
+	dumplistMatch := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{1,5})?.*:?.*`)
 	for _, line := range lines {
 		go func(line string) {
 			if !dumplistMatch.MatchString(line) {
 				log.Println("string formatted incorrectly: " + line)
 				return
 			}
-			hostString := strings.Split(line, " ")[0]
-			hostString = strings.Replace(hostString, " ", "", -1)
-			credString := strings.Split(line, " ")[1]
-			credString = strings.Replace(credString, " ", "", -1)
-			ip := strings.Split(hostString, ":")[0]
-			creds := strings.Split(credString, ":")
-			if len(creds) == 2 {
-				user := creds[0]
-				pass := creds[1]
+			connectionString := strings.Split(line, " ")
+			hostString, credString := func(connectionString []string) (string, string) {
+				if len(connectionString) == 2 {
+					fmt.Println(len(connectionString))
+					hostString := connectionString[0]
+					credString := connectionString[1]
+					hostString = strings.Replace(hostString, " ", "", -1)
+					credString = strings.Replace(credString, " ", "", -1)
+					return hostString, credString
+				}
+				hostString := connectionString[0]
+				return hostString, ""
+			}(connectionString)
+			ip, port := func(hostString string) (string, string) {
+				host := strings.Split(hostString, ":")
+				if len(hostString) == 2 {
+					ip := host[0]
+					port := host[1]
+					return ip, port
+				}
+				ip := host[0]
+				return ip, ""
+			}(hostString)
+			user, pass := func(credString string) (string, string) {
 				if len(Username) > 0 || len(Password) > 0 {
 					user := Username
 					pass := Password
-					TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
-					return
+					return user, pass
 				}
-				TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
-			} else if nouserRE.MatchString(credString) {
-				user := ""
-				pass := creds[0]
-				if len(Username) > 0 || len(Password) > 0 {
-					user := Username
-					pass := Password
-					TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
-					return
+				creds := strings.Split(*Conf.Credentials, ":")
+				if len(*Conf.Credentials) > 0 {
+					if len(creds) > 1 {
+						if nouserRE.MatchString(creds[0]) {
+							user := ""
+							pass := creds[1]
+							return user, pass
+						}
+						if nopassRE.MatchString(creds[1]) {
+							user := creds[0]
+							pass := ""
+							return user, pass
+						}
+						user := creds[0]
+						pass := creds[1]
+						return user, pass
+					}
+					return "", ""
 				}
-				TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
-			} else if nopassRE.MatchString(credString) {
-				user := creds[0]
-				pass := ""
-				if len(Username) > 0 || len(Password) > 0 {
-					user := Username
-					pass := Password
-					TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
-					return
+				creds = strings.Split(credString, ":")
+				if len(credString) > 1 {
+					if nouserRE.MatchString(creds[0]) {
+						user := ""
+						pass := creds[0]
+						return user, pass
+					}
+					if nopassRE.MatchString(creds[1]) {
+						user := creds[0]
+						pass := ""
+						return user, pass
+					}
+					user := creds[0]
+					pass := creds[1]
+					return user, pass
 				}
+				return "", ""
+			}(credString)
+			if len(Port) > 0 {
 				TelnetPreflight(hostString, ip, Port, user, pass, Alert, OutputFile)
+				return
 			}
+			TelnetPreflight(hostString, ip, port, user, pass, Alert, OutputFile)
+			return
 		}(line)
 		time.Sleep(20 * time.Millisecond)
 	}
 }
 
+/*
 func doIPList(lines []string) {
 	listMatch := regexp.MustCompile(`[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{1,5})?(\s+|\t+)?$`)
 	for _, line := range lines {
@@ -148,9 +180,14 @@ func doIPList(lines []string) {
 				pass := ""
 				return user, pass
 			}(credString)
-			TelnetPreflight(line, ip, Port, user, pass, Alert, OutputFile)
+			if len(Port) > 0 {
+				TelnetPreflight(line, ip, Port, user, pass, Alert, OutputFile)
+				return
+			}
+			TelnetPreflight(line, ip, port, user, pass, Alert, OutputFile)
 			return
 		}(line)
 		time.Sleep(400 * time.Millisecond)
 	}
 }
+*/
